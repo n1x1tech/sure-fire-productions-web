963a13a056908af89b57ae73adeef9a87e030589
n1x1tech <nixitechnology@gmail.com>
2025-08-02 20:43:02 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: process.env.NODE_ENV === 'production' ? '/sure-fire-productions-web/' : '/',
   build: {
     rollupOptions: {
       output: {
@@ -19,9 +18,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
