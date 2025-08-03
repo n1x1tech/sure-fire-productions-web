7e1372879cf1c2464f1c276b1860203b739c2466
n1x1tech <nixitechnology@gmail.com>
2025-08-02 20:31:01 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/sure-fire-productions-web/",
+  base: "/",
   build: {
     rollupOptions: {
       output: {
